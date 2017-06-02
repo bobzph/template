cc74c10eee248ddda8afeddbcc8c48c8108ef32b
bobzph <604731831@qq.com>
2017-06-02 09:33:44 +0800

    temp

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('#fullpage').fullpage({
+        'verticalCentered': false,
+        'css3': true,
+        anchors: ['page1', 'page2', 'page3', 'page4','page5','page6','page7'],
+        // 'navigation': true,
+        'navigationPosition': 'right',
+        'navigationTooltips': ['fullPage.js', 'Powerful', 'Amazing', 'Simple']
+    })
+})
